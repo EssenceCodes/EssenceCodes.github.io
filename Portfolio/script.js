9c1ff02c2cfd3e89cb40d0bcdeda675902d4f67e
☰ ssence <7839758+EssenceCodes@users.noreply.github.com>
2023-12-08 00:52:39 -0500

    remove scroll

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -48,15 +48,6 @@ function toggleExtra() {
   }
 }
 
-
-// scroll reveal
-ScrollReveal({ 
-    // reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
-
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
